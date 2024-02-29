18f15c4560607b51380b43d2d80894d6bcfefa39
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-02-29 12:44:59 +0530

    Update Main_module.v

--- Module-2/2 X 1_MUX_AXI/Main_module.v
+++ Module-2/2 X 1_MUX_AXI/Main_module.v
@@ -4,12 +4,13 @@ module Mux_2_1(
 input sel,
 input [7:0]A,B,
 input clk,reset,Tvalid,Tready,Tlast,
-output [7:0]out
+output [7:0]out,
+output [4:0]frame_cnt
 );
 reg [7:0]Hold_data;
- 
+reg [4:0]cnt=5'd0;
 always@(posedge clk,posedge reset) begin
-    if(reset || Tlast) 
+    if(reset ) 
         begin
             Hold_data <= 8'h00;
         end
@@ -22,6 +23,10 @@ always@(posedge clk,posedge reset) begin
         end    
 end
 
-   
+always@(Tlast) begin
+    if(Tlast) cnt <= cnt + 1;
+    else cnt <= cnt;
+end
+assign frame_cnt = cnt;  
 assign out = Hold_data;
 endmodule
