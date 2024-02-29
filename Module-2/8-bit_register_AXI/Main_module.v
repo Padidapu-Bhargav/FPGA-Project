ff04aa8185cb2be97b21342de07c8cec0fcee554
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-02-29 11:10:56 +0530

    Update Main_module.v

--- Module-2/8-bit_register_AXI/Main_module.v
+++ Module-2/8-bit_register_AXI/Main_module.v
@@ -4,14 +4,16 @@ module AXI_8_bit_register(
 input [7:0] in_data,
 input clk,reset,
 input T_valid_in,T_ready,Tlast,
-output [7:0] out_data);
+output [7:0] out_data,
+output [4:0]frame_cnt);
 
+reg [4:0]cnt=5'd0;
 reg [7:0]Hold_data; // it is used for holding the data;
 
 always @(posedge clk , posedge reset) begin
 
    
-    if(reset || Tlast)begin
+    if(reset)begin
         Hold_data <= 8'b00000000;
     end
      
@@ -26,5 +28,11 @@ always @(posedge clk , posedge reset) begin
             end
     end
 end
+
+always@(posedge clk, posedge reset) begin
+    if(Tlast) cnt <= cnt + 5'b1;
+    else cnt <= cnt;
+end
+assign frame_cnt = cnt;
 assign out_data = Hold_data;
 endmodule
