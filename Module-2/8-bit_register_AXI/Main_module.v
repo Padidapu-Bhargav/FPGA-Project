3b28b90d606a752b4335cf5b39dd80b6a68d4658
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-03-04 15:12:05 +0530

    Update Main_module.v

--- Module-2/8-bit_register_AXI/Main_module.v
+++ Module-2/8-bit_register_AXI/Main_module.v
@@ -1,38 +1,58 @@
 `timescale 1ns / 1ps
 
 module AXI_8_bit_register(
-input [7:0] in_data,
-input clk,reset,
-input T_valid_in,T_ready,Tlast,
-output [7:0] out_data,
-output [4:0]frame_cnt);
+    input clk,rst,
+    
+    //master
+    input [7:0]data_in,
+    input m_valid,
+    output reg m_ready,
+    output reg  m_last,
+    
+    //slave
+    output reg [7:0]s_data,
+    output reg s_valid,
+    input s_ready,
+    output reg s_last
+     );
+reg [7:0]hold_data;
+reg valid;   
+always@(clk)begin
+    valid<=m_valid;
+end
 
-reg [4:0]cnt=5'd0;
-reg [7:0]Hold_data; // it is used for holding the data;
+always@(posedge clk) begin
+    s_valid <= m_valid;
+    m_ready <= s_ready;
+end
 
-always @(posedge clk , posedge reset) begin
 
-   
-    if(reset)begin
-        Hold_data <= 8'b00000000;
+always@(posedge clk) begin
+    if(rst) begin
+        s_data <= 8'h00;
     end
-     
     else begin
-        if(T_valid_in && T_ready)
-            begin
-                Hold_data <= in_data;          
-            end
-        else 
-            begin
-                Hold_data <= Hold_data;
-            end
+        s_valid <= m_valid;
+        m_ready <= s_ready;
+        if(m_valid && s_ready) begin
+            s_data <= data_in;
+        end
+        else begin
+             s_data <= s_data;
+            end 
+       
     end
 end
 
-always@(Tlast) begin
-    if(Tlast) cnt <= cnt + 5'b1;
-    else cnt <= cnt;
+always@(clk) begin
+    if({valid,m_valid}==2'b10)begin
+     m_last <= 1'b1 ;
+     s_last <= 1'b1 ;
+     end
+    else begin
+    m_last <= 1'b0 ;
+    s_last <= 1'b0 ;
+    end
 end
-assign frame_cnt = cnt;
-assign out_data = Hold_data;
+
 endmodule
