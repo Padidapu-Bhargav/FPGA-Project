8f9aa381608fd3697e6cc14443503bc987a846cf
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-03-08 13:56:12 +0530

    Update Main_module.v

--- Module-2/8-bit_register_AXI/Main_module.v
+++ Module-2/8-bit_register_AXI/Main_module.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
-module AXI_8_bit_register(
+module AXI_8_bit(
     input clk,rst,
     
     //master
     input [7:0]data_in,
     input m_valid,
     output reg m_ready,
-    output reg  m_last,
+    input  m_last,
     
     //slave
     output reg [7:0]s_data,
@@ -16,43 +16,42 @@ module AXI_8_bit_register(
     output reg s_last
      );
 reg [7:0]hold_data;
-reg valid;   
-always@(clk)begin
-    valid<=m_valid;
-end
+
 
 always@(posedge clk) begin
     s_valid <= m_valid;
     m_ready <= s_ready;
+    s_last <= m_last;
 end
 
+
+always@(posedge clk) begin
+    if(rst) begin
+        hold_data <= 8'h00;
+    end
+    else begin
+        if(m_valid && s_ready) begin
+            hold_data <= data_in;
+        end
+        else begin
+             hold_data <= hold_data;
+        end 
+    end
+end
 
 always@(posedge clk) begin
     if(rst) begin
         s_data <= 8'h00;
     end
     else begin
-        s_valid <= m_valid;
-        m_ready <= s_ready;
-        if(m_valid && s_ready) begin
-            s_data <= data_in;
+        if(s_valid && m_ready) begin
+            s_data <= hold_data;
         end
         else begin
              s_data <= s_data;
-            end 
-       
+        end 
     end
 end
 
-always@(clk) begin
-    if({valid,m_valid}==2'b10)begin
-     m_last <= 1'b1 ;
-     s_last <= 1'b1 ;
-     end
-    else begin
-    m_last <= 1'b0 ;
-    s_last <= 1'b0 ;
-    end
-end
 
 endmodule
