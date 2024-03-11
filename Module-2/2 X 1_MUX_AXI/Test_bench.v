f1b582e7d5fbf7cfdc7aff47732e1ef1afb1dc22
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-03-11 10:26:26 +0530

    Update Test_bench.v

--- Module-2/2 X 1_MUX_AXI/Test_bench.v
+++ Module-2/2 X 1_MUX_AXI/Test_bench.v
@@ -1,57 +1,117 @@
 `timescale 1ns / 1ps
 
 
-module TB();
-reg sel;
-reg [7:0]A,B;
-reg clk,reset,Tvalid,Tready,Tlast;
-wire [7:0]out;
-wire [4:0]frame_cnt;
-Mux_2_1 sample(sel,A,B,clk,reset,Tvalid,Tready,Tlast,out,frame_cnt);
+`timescale 1ns / 1ps
 
- /*task set_inputs(
-        input [7:0] d0, d1,
-        input s,TV,TR,
-        output sel,tv,tr,
-        output [7:0]A,B);
-        begin
-            A <= d0;
-            B <= d1;
-            sel <= s;
-        end
-    endtask*/
-initial begin
-    clk =0;
-    forever #5 clk = ~clk;
-    end
+
+module TB();
+reg clk;
+reg rst;
+reg sel;
     
+//slave-1
+reg [7:0]s_data_1;
+reg s_valid_1;
+wire s_ready_1;
+reg s_last_1;
+        
+//slave -2
+reg [7:0]s_data_2;
+reg s_valid_2;
+wire s_ready_2;
+reg s_last_2;
+        
+ //master
+wire [7:0]m_data;
+wire m_valid;
+reg m_ready;
+wire m_last;
+
+Mux_2_1 sample(.clk(clk),.reset(rst),.sel(sel),
+               .s_data_1(s_data_1),
+               .s_valid_1(s_valid_1),
+               .s_ready_1(s_ready_1),
+               .s_last_1(s_last_1),
+               .s_data_2(s_data_2),
+               .s_valid_2(s_valid_2),
+               .s_ready_2(s_ready_2),
+               .s_last_2(s_last_2),
+               .m_data(m_data),
+               .m_ready(m_ready),
+               .m_valid(m_valid),
+               .m_last(m_last) );
+
 initial begin
-     reset = 0;
-     #50 reset = 1;
-     #10 reset = 0;
-    end
-  
+    clk = 0;
+    forever #10 clk = ~clk;
+end
+
 initial begin
-    Tlast =0;
-    forever #150 Tlast = ~Tlast;
-    end
+    rst = 0;
+    #20 rst = 1;
+    #30 rst = 0;
+end
+
+initial begin
+    s_last_1 = 1'b0;
+    
+    s_last_2 = 1'b0;
+end
+
+initial begin
+     s_data_1 = 8'h23 ;// $random;
+end 
+
+initial begin
+    s_data_2 = 8'h45;//$random;
+end 
+
+initial begin
+    m_ready =1;
+  //  forever #50 m_ready = ~m_ready;
+end
+
+initial begin
+    s_valid_1=0; //m_ready=0;
+     forever #120 s_valid_1 = ~s_valid_1;
+end
+
+
+
+initial begin
+    s_valid_2=0;// m_ready=0;
+     forever #140 s_valid_2 = ~s_valid_2;
+
+end
+
 
 initial begin
     sel =0;
-    forever #10 sel = ~sel;
-    end
-    
+    forever #100 sel = ~sel;
+end  
+
+initial
+begin
+
+s_last_1  = 0;
+#230 s_last_1 = 1;
+#10 s_last_1 =0;
+#230 s_last_1 = 1;
+#10 s_last_1= 0;
+#210 s_last_1 = 1;
+#10 s_last_1 = 0;
+#190 s_last_1 =1;
+#10 s_last_1 = 0;       
+end
+ 
+ 
 initial begin
-  /*  set_inputs(8'h22,8'h33,1'b1,1'b1,1'b1,sel,Tvalid,Tready,A,B);
-    #200; set_inputs(8'h44,8'h55,1'b1,1'b1,1'b1,sel,Tvalid,Tready,A,B);*/
-      A = 8'h22; B = 8'h33 ; Tvalid =1 ; Tready =1 ; 
-   #100   A = 8'h33; B = 8'h44 ; Tvalid =1 ; Tready =0 ;
-   #100   A = 8'h55; B = 8'h66 ; Tvalid =1 ; Tready =1 ;
-   #100   A = 8'h22; B = 8'h33 ; Tvalid =1 ; Tready =1 ;
-   #100   A = 8'h11; B = 8'h44 ; Tvalid =0 ; Tready =0 ;
-   #100   A = 8'h55; B = 8'h88 ; Tvalid =1 ; Tready =0 ;
-   #100   A = 8'h12; B = 8'h56 ; Tvalid =1 ; Tready =1 ;
-   #100   A = 8'h90; B = 8'h15 ; Tvalid =1 ; Tready =1 ;
-      
+s_last_2  = 0;
+#190 s_last_2 = 1;
+#10 s_last_2 = 0;
+#350 s_last_2 = 1;
+#10 s_last_2 =0;
+#230 s_last_2 =1;
+#10 s_last_2 = 0;        
 end
 endmodule
