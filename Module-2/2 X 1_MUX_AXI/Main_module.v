0940c21ea32bfafa508a1ff606c15a862fb48906
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-03-11 10:25:13 +0530

    Update Main_module.v

--- Module-2/2 X 1_MUX_AXI/Main_module.v
+++ Module-2/2 X 1_MUX_AXI/Main_module.v
@@ -1,32 +1,58 @@
 `timescale 1ns / 1ps
 
 module Mux_2_1(
-input sel,
-input [7:0]A,B,
-input clk,reset,Tvalid,Tready,Tlast,
-output [7:0]out,
-output [4:0]frame_cnt
-);
-reg [7:0]Hold_data;
-reg [4:0]cnt=5'd0;
-always@(posedge clk,posedge reset) begin
-    if(reset ) 
-        begin
-            Hold_data <= 8'h00;
+    input clk, reset, sel,
+    
+    // Slave-1
+    input [7:0] s_data_1,
+    input s_valid_1,
+    output reg s_ready_1,
+    input s_last_1,
+    
+    // Slave -2
+    input [7:0] s_data_2,
+    input s_valid_2,
+    output reg s_ready_2,
+    input s_last_2,
+ 
+    // Master
+    output reg [7:0] m_data,
+    input m_ready,
+    output reg m_valid,
+    output reg m_last
+    );        
+
+always @(posedge clk) begin
+    if (reset) begin
+       m_data <= 8'h00;
+    end
+    else begin
+        // If sel = 1, s_data_1 is the input
+        // if sel = 0, s_data_2 is the input
+        if (~sel) begin
+            if (s_valid_1 && m_ready) begin
+                m_data <= s_data_1;
+            end
+            else begin
+                m_data <= m_data;
+            end
         end
-    else 
-        begin
-            if(Tvalid && Tready) 
-                Hold_data = sel ? B : A ;
-            else
-                Hold_data <= Hold_data;
-        end    
+        else begin
+            if (s_valid_2 && m_ready) begin
+                m_data <= s_data_2;
+            end
+            else begin
+                m_data <= m_data;
+            end
+        end  
+    end
 end
 
-always@(Tlast) begin
-    if(Tlast) cnt <= cnt + 1;
-    else cnt <= cnt;
+always @(posedge clk) begin
+        m_valid <= ~sel ? s_valid_1 : s_valid_2;
+        m_last <= ~sel ? s_last_1 : s_last_2;
+        s_ready_1 <= ~sel ? m_ready : 0;
+        s_ready_2 <= ~sel ? 0 : m_ready;
 end
-assign frame_cnt = cnt;  
-assign out = Hold_data;
+
 endmodule
