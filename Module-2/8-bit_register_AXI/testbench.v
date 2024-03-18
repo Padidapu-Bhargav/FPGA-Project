a14c64f146bf63b1f42a6a8a4145aded9b2386ca
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-03-18 12:39:54 +0530

    Update testbench.v

--- Module-2/8-bit_register_AXI/testbench.v
+++ Module-2/8-bit_register_AXI/testbench.v
@@ -4,28 +4,29 @@
 module TB();
 reg clk,rst;
     
-//master
-reg [7:0]data_in;
-reg m_valid;
-wire m_ready;
-reg m_last;
-    
 //slave
-wire [7:0]s_data;
-wire s_valid;
-reg s_ready;
-wire s_last;
+reg [7:0]s_data;
+reg s_valid;
+wire s_ready;
+reg s_last;
+
+    
+//master
+wire [7:0]m_data;
+wire m_valid;
+reg m_ready;
+wire m_last;
 
 
 AXI_8_bit sample(.clk(clk), .rst(rst),
-                          .data_in(data_in),
-                          .m_valid(m_valid),
-                          .m_ready(m_ready),
-                          .m_last(m_last),
                           .s_data(s_data),
                           .s_valid(s_valid),
                           .s_ready(s_ready),
-                          .s_last(s_last)       );
+                          .s_last(s_last),
+                          .m_data(m_data),
+                          .m_valid(m_valid),
+                          .m_ready(m_ready),
+                          .m_last(m_last)       );
 
 
 initial begin
@@ -34,38 +35,101 @@ initial begin
 end
 
 initial begin
-    rst = 0;
-    #10 rst = 1;
-    #10 rst = 0;
+    rst = 1;
+    #20 rst = 0;
 end
 
 initial begin
-    m_last =0;
-    #190 m_last = 1 ;
-    #20 m_last =0;
-    #380 m_last =1;
-    #20 m_last = 0;
-    #280 m_last =1;
-    #20 m_last = 0;
+
+    s_data = 0;
+    s_valid = 0;
+    s_last = 0;
+    repeat(2) @(posedge clk);
+    
+    //1st frame
+    repeat (7) begin
+      @(posedge clk);
+      s_valid = 1;
+      s_data = $random;  
+    end
+    @(posedge clk) s_last = 1;  
+    s_data = $random; 
+    @(posedge clk)s_data = 0;
+    s_valid = 0; 
+    s_last = 0;
+    
+    // 3 clock pulse difference between frame
+    repeat(3) @(posedge clk); 
+    // 2nd frame
+    s_valid = 0;
+    s_last = 0;
+    repeat (7) begin
+      @(posedge clk);
+      s_valid = 1;
+      s_data = $random;   
+    end
+    @(posedge clk) s_last = 1;  
+    s_data = $random; 
+    @(posedge clk)s_data = 0;
+    s_valid = 0; 
+    s_last = 0;
+    
+    //3 clock pulse difference between frames
+    repeat(3) @(posedge clk); 
+    // 3rd frame
+    s_valid = 0;
+    s_last = 0;
+      
+    repeat (7) begin
+      @(posedge clk);
+      s_valid = 0;
+      s_data = $random;   
+    end
+    @(posedge clk) s_last = 1;  
+    s_data = $random; 
+    @(posedge clk)s_data = 0;
+    s_valid = 0; 
+    s_last = 0;
+    
+    // 3 clock pulse delay
+    repeat(3) @(posedge clk); 
+    // 4th frame
+     @(posedge clk)  s_valid =0;
+      s_data = $random;
+     @(posedge clk)  s_valid =0;
+      s_data = $random;
+     @(posedge clk)  s_valid =0;
+      s_data = $random;
+     @(posedge clk)  s_valid =0;
+      s_data = $random;
+      
+    repeat (6) begin
+      @(posedge clk);
+      s_valid = 1;
+      s_data = $random;  
+    end
+    
+    @(posedge clk) s_last = 1;  
+    s_data = $random;
+    @(posedge clk)s_data = 0;
+    s_valid = 0; 
+    s_last = 0;
+    
+              
 end
 
-
 initial begin
-    data_in =0;
-    repeat(100) #50 data_in = $random;
-end   
-
-initial begin
-    m_valid=0; s_ready=0;
-    #110 m_valid=1; s_ready=1;
-    #100 m_valid=1; s_ready=0;
-    #100 m_valid=0; s_ready=1;
-    #100 m_valid=1; s_ready=0;
-    #100 m_valid=1; s_ready=1;
-    #100 m_valid=0; s_ready=1;
-    #100 m_valid=1; s_ready=0;
-    #100 m_valid=1; s_ready=1;
-    #100 m_valid=0; s_ready=1;
+    repeat(11) @(posedge clk) m_ready = 1'b1;  // Master is always ready to receive data
+    repeat(15) @(posedge clk) m_ready = 1'b0;
+    repeat(9) @(posedge clk) m_ready = 1'b1;
+    repeat(3) @(posedge clk) m_ready = 1'b0;
+    repeat(2)@(posedge clk) m_ready = 1'b0;
+    repeat(2) @(posedge clk) m_ready = 1'b1;
+    repeat(2) @(posedge clk) m_ready = 1'b1;
+    repeat(2) @(posedge clk) m_ready = 1'b0;
+    @(posedge clk) m_ready = 1'b1;
+    
 end
+
 
 endmodule
