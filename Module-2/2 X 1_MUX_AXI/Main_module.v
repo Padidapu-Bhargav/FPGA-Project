052144ccb2df9e56a0e9f8ba26c257e3b3448898
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-03-19 12:44:26 +0530

    Update Main_module.v

--- Module-2/2 X 1_MUX_AXI/Main_module.v
+++ Module-2/2 X 1_MUX_AXI/Main_module.v
@@ -20,39 +20,49 @@ module Mux_2_1(
     input m_ready,
     output reg m_valid,
     output reg m_last
-    );        
+    );  
+
+reg last1,last2;      
+always @(posedge clk) begin
+    last1 <= s_last_1;
+    last2 <= s_last_2;
+end
 
 always @(posedge clk) begin
     if (reset) begin
        m_data <= 8'h00;
+       m_valid <= 1'b0;
     end
     else begin
         // If sel = 1, s_data_1 is the input
         // if sel = 0, s_data_2 is the input
-        if (~sel) begin
-            if (s_valid_1 && m_ready) begin
-                m_data <= s_data_1;
+        if (sel) begin
+            if (s_valid_2 && m_ready) begin
+                m_data <= s_data_2;
+                m_valid <= 1'b1;
             end
             else begin
-                m_data <= m_data;
+                m_data <= 8'h00;
+                m_valid <= 1'b0;
             end
         end
         else begin
-            if (s_valid_2 && m_ready) begin
-                m_data <= s_data_2;
+            if (s_valid_1 && m_ready) begin
+                m_data <= s_data_1;
+                m_valid <= 1'b1;
             end
             else begin
-                m_data <= m_data;
+                m_data <= 8'h00;
+                m_valid <= 1'b0;
             end
         end  
     end
 end
 
 always @(posedge clk) begin
-        m_valid <= ~sel ? s_valid_1 : s_valid_2;
-        m_last <= ~sel ? s_last_1 : s_last_2;
-        s_ready_1 <= ~sel ? m_ready : 0;
-        s_ready_2 <= ~sel ? 0 : m_ready;
+        m_last <= reset? 0 :(sel ? last2 : last1);
+        s_ready_1 <= sel ? 0 : m_ready;
+        s_ready_2 <= sel ? m_ready : 0 ;
 end
 
 endmodule
