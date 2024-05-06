b4080b20557b32b65f5731e4c7944a88445d145c
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-05-06 15:06:17 +0530

    Update main module.v

--- Module-3/MAC/main module.v
+++ Module-3/MAC/main module.v
@@ -1,25 +1,30 @@
 `timescale 1ns / 1ps
 
-/////////
-// MAC operation ( a*c + b*c )
-/////////
+///////////////
+// MAC operation ( (a + b)*(c + d) )
+///////////////
 
-module MAC#(parameter Data_width=8)(
-    // slave data a
-    input [Data_width-1:0]a_data, 
-    
-    //slave data b
+module MAC_add#(parameter Data_width=16)(
+    // slave data A
+    input [Data_width-1:0]a_data,
+       
+    //slave data B
     input [Data_width-1:0]b_data,
     
-    //slave data c
+    //slave data C
     input [Data_width-1:0]c_data,
+    
+    //slave data D
+    input [Data_width-1:0]d_data,
     
     //master 
     output reg [Data_width-1:0]m_data
+
     );
     
  always@(*)begin
-   m_data <= (a_data*c_data + b_data*c_data);
+   m_data <= ( (a_data + b_data )*( c_data + d_data) );
  end   
-   
+    
+    
 endmodule
