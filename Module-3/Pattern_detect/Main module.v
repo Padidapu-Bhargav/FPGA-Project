c4f61e27a540f89b86939c05b7980ebf43da639d
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-05-15 14:32:01 +0530

    Update Main module.v

--- Module-3/Pattern_detect/Main module.v
+++ Module-3/Pattern_detect/Main module.v
@@ -2,13 +2,13 @@
 
 module Pattern_detect#(parameter input_1_width=10,
                        parameter input_2_width=10,
-                       parameter output_width=20)(
+                       parameter output_width=20,
+                       parameter pattern=20'd36)(
     input clk,
     input [input_1_width:0] A,
     input [input_2_width:0] B,
-    input [output_width:0] pd_pattern1,
     output  [output_width:0] C,
-    output reg ones_o
+    output reg pattern_detection
 );
 
 reg [output_width:0] ab;
@@ -17,24 +17,21 @@ reg [output_width:0] ab;
 // Pattern detect mask
 reg [input_1_width:0] A1;
 reg [input_2_width:0] B1;
-reg [output_width:0] pd_pattern;
-reg [output_width :0] mask = 20'b11111111111111111111;
+
 
 always @(posedge clk) begin
 A1 <= A;
 B1 <= B;
-pd_pattern <= pd_pattern1;
 end
 
 always @(posedge clk) begin
       
-    ab <= A1 * B1 ; // Perform the multiplication
+    ab <= A1 * B1 ; 
 
-    // Pattern detect logic
-    if (ab == (pd_pattern||mask)) begin
-        ones_o <= 1'b1; // Set ones_o if pattern detected
+    if (ab == pattern ) begin
+        pattern_detection <= 1'b1; 
     end else begin
-        ones_o <= 1'b0; // Reset ones_o if pattern not detected
+        pattern_detection <= 1'b0;
     end
 end
 
