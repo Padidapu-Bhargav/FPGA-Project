ece16756916dbdbca230fe0e302c8f78cd4c11c6
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-05-20 11:00:21 +0530

    Update main module.v

--- Module-3/Assignment-2/main module.v
+++ Module-3/Assignment-2/main module.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 module packet_data #(
-    parameter Data_width = 8,
-    parameter Depth = 15)(
+    parameter Data_width = 16,
+    parameter Depth = 10)(
 
       input clk,
       input rst,
@@ -22,25 +22,21 @@ module packet_data #(
       output  empty
     );
 integer i;
-  reg [Data_width-1:0] mem_1 [Depth-1:0];
-  reg [Data_width-1:0] mem_2 [Depth-1:0];
-  reg  mem_3 [Depth-1:0];
-  reg  store_last [Depth-1:0];
+  reg [Data_width-1:0] mem_data [Depth-1:0]; // array for storing the data
+  reg  mem_valid [Depth-1:0]; // array for storing the vlid signal
+  reg  mem_last [Depth-1:0]; // array for storing the last signal
   reg [Data_width-1:0] wr_ptr1;
   reg [Data_width-1:0] rd_ptr;
   reg [Data_width-1:0] wr_ptr2;
 
-//assign wr_ptr2 = len -k;
 
-//always@(*) m_last = s_last;
 // Updating the storage with zeros
 initial begin
  for(i = 0; i < Depth; i = i + 1)
   begin
-    mem_1[i] = 0;
-    mem_2[i] = 0;
-    mem_3[i] = 0;
-    store_last[i] = 0;
+    mem_data[i] = 0;
+    mem_valid[i] = 0;
+    mem_last[i] = 0;
   end
 end
 
@@ -51,33 +47,30 @@ always@(posedge clk)  begin
 end
 
 assign full = wr_ptr1==Depth?1:0;
-assign empty = wr_ptr1==0?1:0;
+assign empty = wr_ptr2==0?1:0;
 
-// for mem_1
+// for mem_data
 always@(posedge clk)begin
     if (rst)begin
         wr_ptr1 <= 0;
         wr_ptr2 <= 0;
     end 
     else if( s_valid && s_ready) begin
-        mem_1[wr_ptr1] <= s_data;
-        mem_3[wr_ptr1] <= s_last;
-        if(wr_ptr1 == len-1)begin
+        mem_data[wr_ptr1] <= s_data;
+        mem_last[wr_ptr1] <= s_last;
+        if(wr_ptr1 == len-1 || s_last )begin
             wr_ptr1 <= 0;
-            wr_ptr2 <= len-k; 
-           // m_valid <= 1;   
+            wr_ptr2 <= len-k;   
         end
         else wr_ptr1 <= wr_ptr1 + 1;
         if(wr_ptr2 != 0) begin
-            mem_2[wr_ptr2] <= s_data;
-            store_last[wr_ptr2] <= s_data == 0 ? 1'b0 : 1'b1;
+            mem_data[wr_ptr2] <= mem_data[wr_ptr2] + s_data;
+            mem_valid[wr_ptr2] <= s_data == 0 ? 1'b0 : 1'b1;
             if(wr_ptr2 == len-1)begin
-                wr_ptr2 <= 0;
-               // m_valid <= 0;  
+                wr_ptr2 <= 0; 
             end
             else begin
                 wr_ptr2 <= wr_ptr2 + 1;
-                //m_valid <= 1;
             end
         end
         
@@ -86,15 +79,15 @@ end
 
 always@(posedge clk) begin
     if(rst) begin
-        rd_ptr <= 0;
+        rd_ptr <= -(k+2);
         m_valid <= 0;
         m_last <= 0;
         m_data <= 0;
     end
     else if( m_ready)begin
-        m_valid = store_last[rd_ptr];
-        m_last <= m_valid ? mem_3[rd_ptr]:0;
-        m_data <= mem_1[rd_ptr] + mem_2[rd_ptr];
+        m_valid = (rd_ptr >= 0 && rd_ptr < len) ? mem_valid[rd_ptr] :0 ;
+        m_last <= m_valid && (rd_ptr >= 0 && rd_ptr < len)? mem_last[rd_ptr]:0;
+        m_data <= (rd_ptr >= 0 && rd_ptr < len) ? mem_data[rd_ptr] : 0;
         rd_ptr <= rd_ptr == len-1 ? 0 : rd_ptr +1;
     end
     else begin
