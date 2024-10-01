4666521158fad37735ca367db4eb6a49a75043aa
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-10-01 10:21:12 +0530

    Create main.c

---
+++ HPS_DESIGN/Software/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <sys/mman.h>
+#include <sys/time.h> 
+#include <math.h> 
+
+typedef unsigned int uint32_t;
+
+#define AXI_SOC_2_FPGA_BASE_ADDRESS 0x80000000
+#define AXI_SOC_2_FPGA_BASE_ADDRESS_SPAN 0x1
+
+unsigned int *h2p_virtual_base;
+
+unsigned int *h2p_virtual_base_A;
+unsigned int *h2p_virtual_base_B;
+unsigned int *h2p_virtual_base_sel;
+unsigned int *h2p_virtual_base_C;
+unsigned int *h2p_virtual_base_rem;
+
+/*A=0-f
+B=10-1f
+sel=20-2f
+C=30-3f
+rem=40-4f*/
+
+unsigned int *h2p_lw_virtual_base;
+unsigned int *lw_pio_write_ptr ;
+int FPGA_PIO_WRITE = 0x300000;
+int fd;	
+	
+int main()
+{
+
+	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) 	{
+		printf( "ERROR: could not open \"/dev/mem\"...\n" );
+		return( 1 );
+	}
+    
+	h2p_virtual_base_A = mmap( NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000000);
+	h2p_virtual_base_B = mmap( NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000010);
+	h2p_virtual_base_sel = mmap( NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000020);
+	
+	if( (h2p_virtual_base_A == MAP_FAILED)| (h2p_virtual_base_B == MAP_FAILED) | (h2p_virtual_base_sel == MAP_FAILED) ) {
+		printf( "ERROR: mmap1() failed...\n" );
+		close( fd );
+		return(1);
+	}
+
+	//h2p_virtual_base[0] = 0x1;
+	h2p_virtual_base_A[0] = 0x22;
+	h2p_virtual_base_B[0] = 0x32;
+	h2p_virtual_base_sel[0] = 0x0;
+
+	/*printf("Address stored in target ptr: %p\n", h2p_virtual_base);
+	printf("Value pointed to by ptr: %x\n", h2p_virtual_base[0]);*/
+	
+	printf("Address stored in target ptr: %p\n", h2p_virtual_base);
+	printf("Value pointed to by ptr: %x\n", h2p_virtual_base[0]);
+
+	printf("Address stored in target ptr: %p\n", h2p_virtual_base);
+	printf("Value pointed to by ptr: %x\n", h2p_virtual_base[0]);
+
+
+	return( 0 );
+}
