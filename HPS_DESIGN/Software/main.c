17f8dea534e41a0c2c5082b06243dc93477d96f5
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-10-08 12:26:48 +0530

    Update main.c

--- HPS_DESIGN/Software/main.c
+++ HPS_DESIGN/Software/main.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/ipc.h> 
-#include <sys/shm.h> 
 #include <sys/mman.h>
-#include <sys/time.h> 
-#include <math.h> 
 
 typedef unsigned int uint32_t;
 
 #define AXI_SOC_2_FPGA_BASE_ADDRESS 0x80000000
 #define AXI_SOC_2_FPGA_BASE_ADDRESS_SPAN 0x1
-
-unsigned int *h2p_virtual_base;
 
 unsigned int *h2p_virtual_base_A;
 unsigned int *h2p_virtual_base_B;
@@ -23,50 +15,65 @@ unsigned int *h2p_virtual_base_sel;
 unsigned int *h2p_virtual_base_C;
 unsigned int *h2p_virtual_base_rem;
 
-/*A=0-f
-B=10-1f
-sel=20-2f
-C=30-3f
-rem=40-4f*/
+int fd, fd_A, fd_B, fd_sel, fd_C, fd_rem;
 
-unsigned int *h2p_lw_virtual_base;
-unsigned int *lw_pio_write_ptr ;
-int FPGA_PIO_WRITE = 0x300000;
-int fd;	
-	
-int main()
-{
+int main() {
 
-	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) 	{
-		printf( "ERROR: could not open \"/dev/mem\"...\n" );
-		return( 1 );
-	}
-    
-	h2p_virtual_base_A = mmap( NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000000);
-	h2p_virtual_base_B = mmap( NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000010);
-	h2p_virtual_base_sel = mmap( NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000020);
-	h2p_virtual_base_C = mmap( NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000030);
-	h2p_virtual_base_rem = mmap( NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x80000040);
-	if( (h2p_virtual_base_A == MAP_FAILED)| (h2p_virtual_base_B == MAP_FAILED) | (h2p_virtual_base_sel == MAP_FAILED) | (h2p_virtual_base_C == MAP_FAILED) | (h2p_virtual_base_rem == MAP_FAILED)) {
-		printf( "ERROR: mmap1() failed...\n" );
-		close( fd );
-		return(1);
-	}
+    // Open /dev/mem
 
-	//h2p_virtual_base[0] = 0x1;
-	h2p_virtual_base_A[0] = 0x22;
-	h2p_virtual_base_B[0] = 0x32;
-	h2p_virtual_base_sel[0] = 0x0;
-
-	/*printf("Address stored in target ptr: %p\n", h2p_virtual_base);
-	printf("Value pointed to by ptr: %x\n", h2p_virtual_base[0]);*/
-	
-	/*printf("Address stored in target C ptr: %p\n", h2p_virtual_base_C);
-	printf("Value pointed to by C ptr: %x\n", h2p_virtual_base_C[0]);
-
-	printf("Address stored in target rem ptr: %p\n", h2p_virtual_base_rem);
-	printf("Value pointed to by rem ptr: %x\n", h2p_virtual_base_rem[0]);*/
+    // A port
+    if ((fd_A = open("/dev/mem", (O_RDWR | O_SYNC))) == -1) {
+        perror("ERROR: could not open \"/dev/mem\"");
+        return 1;
+    }
 
 
-	return( 0 );
+    h2p_virtual_base_A = mmap(NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd_A, 0x80000000);
+    if (h2p_virtual_base_A == MAP_FAILED) {
+        perror("ERROR: mmap() failed for A");
+        close(fd_A);
+        return 1;
+    }
+
+    //B port
+
+    h2p_virtual_base_B = mmap(NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd_A, 0x80010000);
+    if (h2p_virtual_base_B == MAP_FAILED) {
+        perror("ERROR: mmap() failed for B");
+        close(fd_A);
+        return 1;
+    }
+
+    // sel port
+
+    h2p_virtual_base_sel = mmap(NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd_A, 0x80020000);
+    if (h2p_virtual_base_sel == MAP_FAILED) {
+        perror("ERROR: mmap() failed for sel");
+        close(fd_A);
+        return 1;
+    }
+
+
+    // Input hexadecimal values
+    unsigned int hexValue_A, hexValue_B, hexValue_sel;
+
+    printf("Enter a hexadecimal value for A: ");
+    scanf("%x", &hexValue_A);
+
+    printf("Enter a hexadecimal value for B: ");
+    scanf("%x", &hexValue_B);
+
+    printf("Enter a hexadecimal value for sel: ");
+    scanf("%x", &hexValue_sel);
+
+    // Write to mapped memory
+    h2p_virtual_base_A[0] = hexValue_A;
+    h2p_virtual_base_B[0x10/4] = hexValue_B;
+
+    h2p_virtual_base_sel[0x20/4] = hexValue_sel;
+
+
+
+    close(fd_A);
+    return 0;
 }
