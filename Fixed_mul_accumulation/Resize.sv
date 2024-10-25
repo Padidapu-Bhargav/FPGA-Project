96cb12fb43493948648573f8acf9cc6197783bd1
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2024-10-25 10:38:48 +0530

    Update Resize.sv

--- Fixed_mul_accumulation/Resize.sv
+++ Fixed_mul_accumulation/Resize.sv
@@ -5,7 +5,7 @@ module Resize#(parameter  WI1 = 5,           // integer part bitwidth for intege
                      ) (
                      input clk,
                      input reset,
-                     input signed [ (WI1+WF1)-1 :0]Data,
+                     input signed [ (WI1+WF1)-1 :0]input_Data,
                      output signed [ (WIO+WFO)-1 :0]final_Data,
                      input OF_saturation,
                      output overflow,
@@ -13,7 +13,7 @@ module Resize#(parameter  WI1 = 5,           // integer part bitwidth for intege
                      output underflow);
     
 
-
+reg signed [ (WI1+WF1)-1 :0]Data;
 // temporary register for calculation of SUM
 reg [WI1-1:0]Data_I;  // integer part of product
 reg [WF1-1:0]Data_F;  // fractional part of product
@@ -29,10 +29,12 @@ reg UF_internal;
 ///////////////////////////    
 always @(*) begin
     if(reset) begin
+        Data  = 'd0;
         Data_I = 'd0;
         Data_F = 'd0;
     end
     else begin
+        Data = input_Data;
         Data_I = Data[(WI1+WF1-1):WF1];
         Data_F = Data[(WF1-1):0];
     end
@@ -43,13 +45,13 @@ end
 always@(*) begin
    if(reset) OF_internal = 0;
    else begin
-       if(WIO>WI1) OF_internal =0;
+       if(WIO>WI1) OF_internal = 0;
        else if(Data[WI1+WF1-1]==0)
-           OF_internal =|Data[WI1+WF1-1: WF1+WIO-1];
+           OF_internal = |Data[WI1+WF1-1: WF1+WIO-1];
        else if(Data[WI1+WF1-1]==1)
-           OF_internal =(~(&Data[WI1+WF1-1: WF1+WIO-1]));
+           OF_internal  = (~(&Data[WI1+WF1-1: WF1+WIO-1]));
        else
-           OF_internal =0;
+           OF_internal = 0;
    end    
 end
 
@@ -57,8 +59,8 @@ end
 always@(*) begin
     if(reset) UF_internal = 'd0;
     else begin
-        if((WFO != WF1) && (WF1 > WFO )) UF_internal = |Data_F[WF1-WFO-1:0];
-	    else UF_internal = 0;
+        if((WFO != WF1) && (WF1 > WFO )) UF_internal = |Data[WF1-WFO-1:0];
+	    else UF_internal =  0;
     end
 
 end
@@ -73,7 +75,7 @@ always @(*) begin
             else Data_temp_I = {Data_I[WI1-1],{(WIO-1){1'b0}}};
         end 
         else begin
-            if (WI1 > WIO) Data_temp_I = {Data_I[WI1-1],Data_I[ WIO-1 :0]};
+            if (WI1 > WIO) Data_temp_I = {Data_I[WI1-1],Data_I[ WIO-2 :0]};
             else Data_temp_I = { {(WIO-WI1) {Data_I[WI1-1]} },Data_I};
         end
     end
